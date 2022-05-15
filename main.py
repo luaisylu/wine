f5ccb21b77841a7124b8d4b5338d9dbebe211b3a
Aisylu <79210517+luaisylu@users.noreply.github.com>
2022-05-15 12:33:15 +0300

    Исправила переменные

--- main.py
+++ main.py
@@ -7,23 +7,23 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
 def main():
-    basis = datetime.date(year=1920, month=1, day=1)
-    current_date = basis.year
+    year = datetime.date(year=1920, month=1, day=1)
+    current_date = year.year
     day_the_week = datetime.datetime.today()
     this_year = day_the_week.year
     age = this_year-current_date
-    wine_file = pandas.read_excel('wine3.xlsx', na_values=['N/A', 'NA'], keep_default_na=False).to_dict(orient='record')
+    reading_file = pandas.read_excel('wine3.xlsx', na_values=['N/A', 'NA'], keep_default_na=False).to_dict(orient='record')
     file = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
     )
     template = file.get_template('template.html')
     group_wines = defaultdict(list)
-    for wine_files in wine_file:
-       group_wines[wine_files['Категория']].append(wine_files)
+    for reading_files in reading_file:
+       group_wines[reading_files['Категория']].append(wine_files)
     rendered_page = template.render(
         age=age,
-        wines=wine_file,
+        wines=reading_file,
         group_wines=group_wines
     )
     with open('index.html', 'w', encoding="utf8") as file:
