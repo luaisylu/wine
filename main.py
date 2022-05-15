57ca601b0f3c81a4a845504bed1b975bba2f6f79
Aisylu <79210517+luaisylu@users.noreply.github.com>
2022-05-15 13:09:36 +0300

    Сократила код

--- main.py
+++ main.py
@@ -7,11 +7,10 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
 def main():
-    year = datetime.date(year=1920, month=1, day=1)
-    current_date = year.year
     day_the_week = datetime.datetime.today()
     this_year = day_the_week.year
-    age = this_year-current_date
+    year = 1920
+    age = this_year-year
     reading_file = pandas.read_excel('wine3.xlsx', na_values=['N/A', 'NA'], keep_default_na=False).to_dict(orient='record')
     file = Environment(
         loader=FileSystemLoader('.'),
@@ -20,7 +19,7 @@ def main():
     template = file.get_template('template.html')
     wines_group = defaultdict(list)
     for reading_files in reading_file:
-        wines_group[reading_file['Категория']].append(reading_files)
+        wines_group[reading_files['Категория']].append(reading_files)
     rendered_page = template.render(
         age=age,
         wines=reading_file,
